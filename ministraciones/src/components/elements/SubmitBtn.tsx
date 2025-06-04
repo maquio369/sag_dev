4eff70894c3cf189de2a3da19799fd33c6e20dfa
jperex <jperex0002@gmail.com>
2025-06-04 17:41:14 -0600

    feat: update SubmitBtn class

--- ministraciones/src/components/elements/SubmitBtn.tsx
+++ ministraciones/src/components/elements/SubmitBtn.tsx
@@ -1,36 +1,124 @@
+"use client";
 import { useFormStatus } from "react-dom";
 
+/***
+ * Ejemplo con Form onSubmit. Se utilizará parametro/variable: isPending
+ *      <form onsubmit="myFunction()">
+ * Ejemplo configurado con Form action. Se usara el useFormStatus: pending
+ *      <Form action={async (formData) => {
+ *         const errorMsg = await handleSubmit(formData);
+ *         setError(errorMsg as string);
+ *       }}>
+ */
 const SubmitBtn = function ({
   text = "Guardar",
   icon = "fa-floppy-disk",
-  className = "",
+  className,
+  isPending,
 }: {
   text?: string;
   icon?: string;
   className?: string;
+  isPending?: boolean;
 }) {
-  /* Guardar,fa-floppy-disk Eliminar,fa-trash-can Enviar,fa-paper-plane Aceptar,fa-check */
   const { pending } = useFormStatus();
+  isPending = isPending === undefined ? pending : isPending;
+
   return (
-    <button className={"btn1 " + className} disabled={pending}>
+    <button className={"btn1 " + className} disabled={isPending}>
       <i
-        className={"mr-1 fa-solid " + icon + (pending ? " fa-beat-fade" : "")}
+        className={
+          "mr-1.5 fa-solid " + icon + (isPending ? " fa-beat-fade" : "")
+        }
       />
       {text}
     </button>
   );
 };
 
-SubmitBtn.Save = function () {
-  return <SubmitBtn text="Guardar" icon="fa-floppy-disk" className="mt-2" />;
+SubmitBtn.Enter = function ({
+  className,
+  isPending,
+}: {
+  className?: string;
+  isPending?: boolean;
+}) {
+  return (
+    <SubmitBtn
+      text="Entrar"
+      icon="fa-solid fa-arrow-right-to-bracket"
+      className={"mt-2 " + className}
+      isPending={isPending}
+    />
+  );
 };
-SubmitBtn.Delete = function () {
-  return <SubmitBtn text="Eliminar" icon="fa-trash-can" className="mt-2" />;
+
+SubmitBtn.Save = function ({
+  className,
+  isPending,
+}: {
+  className?: string;
+  isPending?: boolean;
+}) {
+  return (
+    <SubmitBtn
+      text="Guardar"
+      icon="fa-floppy-disk"
+      className={"mt-2 " + className}
+      isPending={isPending}
+    />
+  );
 };
-SubmitBtn.Send = function () {
-  return <SubmitBtn text="Enviar" icon="fa-paper-plane" className="mt-2" />;
+
+SubmitBtn.Delete = function ({
+  className,
+  isPending,
+}: {
+  className?: string;
+  isPending?: boolean;
+}) {
+  return (
+    <SubmitBtn
+      text="Eliminar"
+      icon="fa-trash-can"
+      className={"mt-2 " + className}
+      isPending={isPending}
+    />
+  );
 };
-SubmitBtn.Accept = function () {
-  return <SubmitBtn text="Aceptar" icon="fa-check" className="mt-2" />;
+
+SubmitBtn.Send = function ({
+  className,
+  isPending,
+}: {
+  className?: string;
+  isPending?: boolean;
+}) {
+  return (
+    <SubmitBtn
+      text="Enviar"
+      icon="fa-paper-plane"
+      className={"mt-2 " + className}
+      isPending={isPending}
+    />
+  );
 };
+
+SubmitBtn.Accept = function ({
+  className,
+  isPending,
+}: {
+  className?: string;
+  isPending?: boolean;
+}) {
+  return (
+    <SubmitBtn
+      text="Aceptar"
+      icon="fa-check"
+      className={"mt-2 " + className}
+      isPending={isPending}
+    />
+  );
+};
+/* Guardar,fa-floppy-disk Eliminar,fa-trash-can Enviar,fa-paper-plane Aceptar,fa-check Entrar,fa-arrow-right-to-bracket */
 export default SubmitBtn;
