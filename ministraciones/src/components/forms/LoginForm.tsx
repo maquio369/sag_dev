ad95a357e513cc76325b763d1ef16a88cd366ecb
jperex <jperex0002@gmail.com>
2025-06-04 18:57:13 -0600

    feat: Set session token when login

--- ministraciones/src/components/forms/LoginForm.tsx
+++ ministraciones/src/components/forms/LoginForm.tsx
@@ -1,81 +1,41 @@
 "use client";
+
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import { ToastContainer, ToastOptions, toast } from "react-toastify";
+import SubmitBtn from "../elements/SubmitBtn";
+import { handleSubmit } from "@/components/forms/actions";
+//import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  const router = useRouter();
+  //const router = useRouter();
 
   const toastOptions = {
     theme:
       typeof window !== "undefined" ? localStorage.getItem("theme") : "dark",
   } as ToastOptions;
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // Validación básica
-    if (!username.trim() || !password.trim()) {
-      setError("Por favor, ingrese usuario y contraseña");
-      return;
-    }
-
-    setIsLoading(true);
-    setError("");
-
-    try {
-      console.log("Iniciando proceso de login para usuario:", username);
-      var apiHost =
-        process.env.API_URL !== undefined ? process.env.API_URL : "";
-      apiHost = "http://172.16.35.43:3011/";
-
-      // Realizar petición al endpoint de login
-      const response = await fetch(apiHost + "api/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ usuario: username, clave: password }),
-      });
-
-      const data = await response.json();
-      console.log("Respuesta del servidor:", data);
-sessionStorage.setItem("auth_token", data.token);
-          console.log(data.message);
-          sessionStorage.setItem("userData", data.message);
-      if (data && "token" in data) {
-        if (data.token != "") {
-          console.log("Ok...");          
-          //ok
-          window.location.href = "/sistemas";
-        } else {
-          setError(data.message);
-
-          //throw new Error("No se recibió token de autenticación");
-        }
-      }
-    } catch (err: any) {
-      console.log("Error autorizando inicio de sesión");
-      setError("Error autorizando inicio de sesión");
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col space-y-6">
-      {error && (
-        <div className="p-3 bg-red-100 dark:bg-red-900/30 text-red-700 dark:text-red-200 rounded-md text-sm">
-          <div className="flex items-center">
-            <i className="fa-solid fa-circle-exclamation mr-2"></i>
-            <span>{error}</span>
-          </div>
-        </div>
-      )}
-
+    /*< form onSubmit={handleSubmit} className="flex flex-col space-y-6" > */
+    //<form action={(e) => {  handleSubmit(); }}>
+    <form
+      className="flex flex-col space-y-6"
+      action={async (formData) => {
+        const jsonData = await handleSubmit(formData);
+        if (jsonData) {
+          sessionStorage.setItem("token", jsonData.token);
+          sessionStorage.setItem("message", jsonData.message);
+          if (jsonData.token===undefined || jsonData.token === "") {
+            setError(jsonData.message as string);
+          } else {
+            window.location.href = "/sistemas";
+          }
+        }
+      }}
+    >
       <div className="flex flex-col space-y-1">
         <span className="lbl">Usuario</span>
         <input
@@ -109,21 +69,17 @@ sessionStorage.setItem("auth_token", data.token);
       </div>
 
       <div className="items-center gap-2 ">
-        <button type="submit" className="w-full btn1" disabled={isLoading}>
-          {isLoading ? (
-            <>
-              <i className="fa-solid fa-circle-notch fa-spin"></i>
-              <span className="pl-2 font-medium">Verificando...</span>
-            </>
-          ) : (
-            <>
-              <i className="fa-solid fa-arrow-right-to-bracket"></i>
-              <span className="pl-2 font-medium">Entrar</span>
-            </>
-          )}
-        </button>
+        <SubmitBtn.Enter className="w-full" />
       </div>
       <ToastContainer />
+      {error && (
+        <div className="p-3 bg-red-100 dark:bg-red-900/30 text-red-700 dark:text-red-200 rounded-md text-sm">
+          <div className="flex items-center">
+            <i className="fa-solid fa-triangle-exclamation mr-2"></i>
+            <span>{error}</span>
+          </div>
+        </div>
+      )}
     </form>
   );
 };
---
+++ ministraciones/src/components/forms/actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+export async function handleSubmit(formData: FormData): Promise<any> {
+  let setError = "";
+  var data;
+  try {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    if (!username || !password) {
+      setError = "Por favor, ingrese usuario y contraseña";
+    } else {
+      console.log("Iniciando proceso de login para usuario:", username);
+      var apiHost =
+        process.env.API_URL !== undefined ? process.env.API_URL : "";
+      //console.log(apiHost);
+
+      const response = await fetch(apiHost + "api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          usuario: username,
+          clave: password,
+        }),
+      });
+
+      data = await response.json();
+      if (data && "token" in data && data.token === "") {
+        setError = data.message; //console.log(data.message);
+      }
+    }
+  } catch (err: any) {
+    console.log("Error autorizando inicio de sesión", err.message);
+    setError = "Error autorizando inicio de sesión";
+  } finally {
+    if (setError != "") {
+      data = { message: setError, token: "" };
+    }
+    return data;
+  }
+}
+
+export async function handleSubmitExample() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return;
+}
