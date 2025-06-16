8ed992c6fb883ad82747b3895169993967342358
jperex <jperex0002@gmail.com>
2025-06-15 23:11:44 -0600

    feat: add api/opciones/getmenu to backend

--- ministraciones_bk/src/api/opciones/opciones.controller.ts
+++ ministraciones_bk/src/api/opciones/opciones.controller.ts
@@ -1,18 +1,35 @@
-import { Controller } from "@nestjs/common";
-import { BaseController } from "src/commons/controller.commons";
-import { BaseService } from "src/commons/service.commons";
-import { Opciones } from "./opciones.entity";
-import { OpcionesService } from "./opciones.service";
+import { Controller, Get, Param } from '@nestjs/common';
+import { BaseController } from 'src/commons/controller.commons';
+import { BaseService } from 'src/commons/service.commons';
+import { Opciones } from './opciones.entity';
+import { OpcionesService } from './opciones.service';
+import { OpcionesSql } from './opciones.sql';
 
 @Controller('api/opciones')
 export class OpcionesController extends BaseController<Opciones> {
+  constructor(
+    private readonly opcionesService: OpcionesService,
+    private readonly opcionesSql: OpcionesSql,
+  ) {
+    super();
+  }
 
-    constructor(private readonly opcionesService: OpcionesService) {
-        super();
-    }
+  getService(): BaseService<Opciones> {
+    return this.opcionesService;
+  }
 
-    getService(): BaseService<Opciones> {
-        return this.opcionesService;
-    }
-
+  @Get('getmenu/:idSistema/:idRol') //localhost:3011/api/opciones/getmenu/2/5
+  async getMenu(
+    @Param('idSistema') idSistema: number,
+    @Param('idRol') idRol: number,
+  ) {
+    return await this.opcionesSql.getMenu(idSistema, idRol);
+  }
+  @Get('getopciones/:idSistema/:idRol')
+  async getOpciones(
+    @Param('idSistema') idSistema: number,
+    @Param('idRol') idRol: number,
+  ) {
+    return await this.opcionesSql.getOpciones(idSistema, idRol);
+  }
 }
--- ministraciones_bk/src/api/opciones/opciones.module.ts
+++ ministraciones_bk/src/api/opciones/opciones.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OpcionesController } from './opciones.controller';
 import { Opciones } from './opciones.entity';
 import { OpcionesService } from './opciones.service';
+import { OpcionesSql } from './opciones.sql';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Opciones])],
-    providers: [OpcionesService],
+    providers: [OpcionesService, OpcionesSql],
     controllers: [OpcionesController]
 })
 export class OpcionesModule {
