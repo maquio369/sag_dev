aec152581a03f7868f473375f6807ce64a3cebe1
jperex <jperex0002@gmail.com>
2025-06-23 11:06:33 -0600

    feat: add separator and tooltiptext to menu options

--- ministraciones/src/components/Menu.tsx
+++ ministraciones/src/components/Menu.tsx
@@ -59,54 +59,66 @@ const Menu = ({ expanded = false, submenu_expanded = true }: Props) => {
 
   return (
     <div className="mt-0 text-sm px-2 ">
-      {menusItems && menusItems.map((i) => (
-        <div className="flex flex-col gap-2" key={i.id_opcion}>
-          <Link
-            className={
-              i.titulo.length > 0
-                ? "text-menuTextoSeparador hover:text-textoEncabezadoTrans font-light pt-3 flex items-center gap-2 focus:outline-none"
-                : "pt-2 text-menuTextoSeparador"
-            }
-            onClick={
-              i.titulo.length > 0 ? () => toggleItem(i.id_opcion) : undefined
-            }
-            href=""
-            hidden={!i.titulo || !expanded}
-            tabIndex={i.titulo ? 0 : -1}
-          >
-            {i.titulo}
-            {i.titulo && (
-              <span
-                className={`ml-auto transition-transform duration-200 ease-in-out ${
-                  openItems[i.id_opcion] ? "-rotate-270" : ""
-                }`}
-                style={{ display: "inline-flex" }}
-              >
-                <i className="fa-solid fa-chevron-right" />
-              </span>
-            )}
-          </Link>
-          {openItems[i.id_opcion] &&
-            i.items.map((item) => (
+      {menusItems &&
+        menusItems.map((i) => (
+          <div className="flex flex-col gap-2" key={i.id_opcion}>
+            {i.titulo ? (
               <Link
-                href={item.link}
-                key={item.opcion}
-                className="flex items-center justify-start gap-2 text-menuTexto py-2 rounded-md hover:bg-menuFondoOpcion hover:text-menuTextoHover"
+                className={
+                  i.titulo.length > 0
+                    ? "text-menuTextoSeparador hover:text-textoEncabezadoTrans font-light pt-3 flex items-center gap-2 focus:outline-none"
+                    : "pt-2 text-menuTextoSeparador"
+                }
+                onClick={
+                  i.titulo.length > 0
+                    ? () => toggleItem(i.id_opcion)
+                    : undefined
+                }
+                href=""
+                title={!expanded? i.titulo:""} 
+                //hidden={Boolean(i.titulo) && !expanded}
+                tabIndex={i.titulo ? 0 : -1}                 
               >
-                <i
-                  className={item.icono.concat(
-                    " text-menuIcon hover:text-menuIconHover ml-2 text-lg"
-                  )}
-                />
-                {expanded && <span>{item.opcion}</span>}
+                {i.titulo && expanded? i.titulo:'...'}
+                {i.titulo && (
+                  <span
+                    className={`ml-auto transition-transform duration-200 ease-in-out ${
+                      openItems[i.id_opcion] ? "-rotate-270" : ""
+                    }`}
+                    style={{ display: "inline-flex" }}
+                  >
+                    <i className="fa-solid fa-chevron-right" />
+                  </span>
+                )}
               </Link>
-            ))}
-        </div>
-      ))}
+            ) : (
+              <div className="separador"></div>
+            )}
+
+            {openItems[i.id_opcion] &&
+              i.items.map((item) => (
+                <Link
+                  href={item.link}
+                  title={!expanded? item.opcion:""} 
+                  key={item.opcion}
+                  className="flex items-center justify-start gap-2 text-menuTexto py-2 rounded-md hover:bg-menuFondoOpcion hover:text-menuTextoHover"
+                >
+                  <i
+                    className={item.icono.concat(
+                      " text-menuIcon hover:text-menuIconHover ml-2 text-lg"
+                    )}
+                  />
+                  {expanded && <span>{item.opcion}</span>}
+                </Link>
+              ))}
+          </div>
+        ))}
+      <div className="separador"></div>
       <Link
         href="/login"
+        title={!expanded? "Salir":""} 
         key="9999"
-        className="flex items-center justify-start gap-2 text-menuTexto py-2 rounded-md hover:bg-menuFondoOpcion hover:text-menuTextoHover mt-4"
+        className="flex items-center justify-start gap-2 text-menuTexto py-0.5 rounded-md hover:bg-menuFondoOpcion hover:text-menuTextoHover mt-4"
       >
         <i className="fa-solid fa-arrow-right-from-bracket text-menuIcon hover:text-menuIconHover ml-2 text-lg" />
         {expanded && <span>Salir</span>}
