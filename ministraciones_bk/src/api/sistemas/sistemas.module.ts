92f12aae55db156b54c1ab1f1e9431ea0f3d294a
maquio369 <maquio92@gmail.com>
2025-08-20 11:39:41 -0600

    Create sistemas.module.ts

---
+++ ministraciones_bk/src/api/sistemas/sistemas.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SistemasController } from './sistemas.controller';
+import { Sistemas } from './sistemas.entity';
+import { SistemasService } from './sistemas.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Sistemas])],
+    providers: [SistemasService],
+    controllers: [SistemasController]
+})
+export class SistemasModule {
+
+}
