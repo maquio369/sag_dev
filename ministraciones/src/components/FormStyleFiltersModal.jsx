40816de317013f49fdbc84e9fa0d8580168cecab
maquio369 <maquio92@gmail.com>
2025-08-20 12:51:09 -0600

    Update FormStyleFiltersModal.jsx

--- ministraciones/src/components/FormStyleFiltersModal.jsx
+++ ministraciones/src/components/FormStyleFiltersModal.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Modal from "@/components/elements/Modal";
 import { getColumnDisplayName } from "@/utils/util";
-import { apiService } from "../services/api";
+import apiService from "../services/api";
 
 /*
 type SchemaColumn = {
